19bed8aa028635225b73440ce360766adb0ebe1f
Daniel Aires Pereira <danielairespereira1411@gmail.com>
2026-08-19 21:31:55 -0300

    pequeno ajuste de print

--- Projeto/Treino.java
+++ Projeto/Treino.java
@@ -59,10 +59,10 @@ public class Treino implements Serializable {
                 String linha;
                 if (ex instanceof ExercicioForca) {
                     ExercicioForca ef = (ExercicioForca) ex;
-                    linha = nomeDivisao + " |" + ef.getNome() + " |" + ef.getSeries() + "| Forca |" + ef.getCargaKg() + " kg x " + ef.getRepeticoes() + "reps";
+                    linha = nomeDivisao + " :" + ef.getNome() + " |" + ef.getSeries() + "| Forca |" + ef.getCargaKg() + " kg x " + ef.getRepeticoes() + "reps";
                 } else {
                     ExercicioCardio ec = (ExercicioCardio) ex;
-                    linha = nomeDivisao + " |" + ec.getNome() + " |" + ec.getSeries() + "| Cardio|" + ec.getTempoMinutos() + " min";
+                    linha = nomeDivisao + " :" + ec.getNome() + " |" + ec.getSeries() + "| Cardio|" + ec.getTempoMinutos() + " min";
                 }
                 writer.write(linha);
                 writer.newLine();           
